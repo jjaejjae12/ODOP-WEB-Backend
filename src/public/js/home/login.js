a7a528f6116a62e67cd97db518853cf29d226535
songjeongmin <ssong6299595@gmail.com>
2021-12-01 14:20:56 +0900

    2021.12.01

--- src/public/js/home/login.js
+++ src/public/js/home/login.js
@@ -1,4 +1,22 @@
 "use strict"
 
-console.log("hello");
-console.log("bye");
+// DOM -> Document Object Model
+
+const id = document.querySelector("#id"),
+    pw = document.querySelector("#password"),
+    // loginBtn = document.querySelector("#loginBtn");
+    loginBtn = document.querySelector("button");
+
+console.log(id);
+console.log(pw);
+
+loginBtn.addEventListener("click", ()=>{
+
+    const req = {
+        id : id.value,
+        pw : pw.value,
+    };
+    console.log(req);
+   
+});
+
