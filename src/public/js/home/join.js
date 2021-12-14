06b01c788ae0acb9479f2e2cbb1ab52cb2075f41
songjeongmin <ssong6299595@gmail.com>
2021-12-14 17:04:58 +0900

    cookie-parser, express-session, bcrypt install

--- src/public/js/home/join.js
+++ src/public/js/home/join.js
@@ -1,1 +1,31 @@
 "use strict"
+
+const id = document.querySelector("#login_id"),
+    pw = document.querySelector("#login_pw"),
+    loginBtn = document.querySelector("#login_submit");
+
+loginBtn.addEventListener("click", ()=>{
+    const req = {
+        id : id.value,
+        pw : pw.value,
+    };
+
+    console.log(req);
+    console.log(JSON.stringify(req));
+   
+    fetch("/login", {
+        method: "POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(req),
+    }).then((res)=>res.json())
+    .then((res)=>{
+        if(res.success){
+            location.href = "/main";
+        }else{
+            alert("로그인 실패");
+            location.href = "/login";
+        }
+    });
+});
