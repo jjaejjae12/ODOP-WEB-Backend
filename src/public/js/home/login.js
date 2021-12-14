460dc09d8161aae03228eecc556bf8108e954df5
songjeongmin <ssong6299595@gmail.com>
2021-12-14 15:49:25 +0900

    로그인

--- src/public/js/home/login.js
+++ src/public/js/home/login.js
@@ -9,32 +9,6 @@ const id = document.querySelector("#login_id"),
     pw = document.querySelector("#login_pw"),
     loginBtn = document.querySelector("#login_submit");
 
-loginBtn.addEventListener("click", ()=>{
-        const req = {
-            id : id.value,
-            pw : pw.value,
-        };
-    
-        console.log(req);
-        console.log(JSON.stringify(req));
-       
-        fetch("/login", {
-            method: "POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body: JSON.stringify(req),
-        }).then((res)=>res.json())
-        .then((res)=>{
-            if(res.success){
-                location.href = "/";
-            }else{
-                console.log(res);
-                
-            }
-        });
-    });
-
 console.log(id);
 console.log(pw);
 
