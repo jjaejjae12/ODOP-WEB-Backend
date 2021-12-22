ae4c30ef09861e3cf505806ae27bd29be35e1225
songjeongmin <ssong6299595@gmail.com>
2021-12-22 10:38:43 +0900

    ddsaf

--- bin/www.js
+++ bin/www.js
@@ -4,6 +4,6 @@ const app = require("../app");
 const PORT = 3000;
 const hostname = '10.120.74.83'
 
-app.listen(PORT, hostname, ()=>{
+app.listen(PORT, ()=>{
     console.log("[SERVER] start 3000");
 });
